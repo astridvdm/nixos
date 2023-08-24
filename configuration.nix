5e91af74759f8c414dfec4e47b3c36a575f71de2
Max van der Merwe <git@maxvdm.com>
2023-08-24 19:55:18 +0200

    add git.maxvdm.com ssh config matchblock.

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,12 @@
          port = 2222;
          identityFile = "/home/max/.ssh/max-git";
          };
+         "git.maxvdm.com" = {
+         hostname = "10.0.0.3";
+         user = "git";
+         port = 2202;
+         identityFile = "/home/max/.ssh/max-git";
+         };
        };
      };
      programs.zsh.enable = true;
