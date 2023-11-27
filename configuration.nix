00c2c69055458665c3f560c6bbce80f203481a1e
Max van der Merwe <git@maxvdm.com>
2023-11-27 19:24:20 +0200

    Fix zen kernel parameter

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
 
   # Networking
@@ -166,7 +167,6 @@
      #iperf3
      #fio
      #smartmontools
-     linuxKernel.kernels.linux_zen # Zen Kernel
    ];
 
   # Enable cron service
