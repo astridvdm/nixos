24fcb62e46ca6a7c36c110f73754b62b92700869
Max van der Merwe <git@maxvdm.com>
2023-12-12 23:04:38 +0200

    fix typo

--- hosts/ion/hardware-configuration.nix
+++ hosts/ion/hardware-configuration.nix
@@ -65,13 +65,11 @@
   security.polkit.enable = true;
 
   # Enable the X11/Wayland windowing system.
-  services.xserver.enable = true
+  services.xserver.enable = true;
 
   # Enable the Gnome Desktop
-  services.xserver.displayManager = {
-    gdm.enable = true;
-    desktopManager.gnome.enable = true;
-  };
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   environment.gnome.excludePackages = (with pkgs; [
     #gnome-photos
