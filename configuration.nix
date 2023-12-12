1661ad69bf89c84d5e288b0714b279dddbaeaa20
Max van der Merwe <git@maxvdm.com>
2023-12-12 14:54:43 +0200

    Add OBS Virtual Cam

--- configuration.nix
+++ configuration.nix
@@ -215,6 +215,16 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # OBS Virtual Cam
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
+
   # NixOS Optimise
   boot.loader.systemd-boot.configurationLimit = 10;
   # boot.loader.grub.configurationLimit = 10;
