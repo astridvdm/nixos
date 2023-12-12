7f5f31841e21e5605d0e5e1522a2c7da3162be9f
Max van der Merwe <git@maxvdm.com>
2023-12-12 22:25:32 +0200

    update git config

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -8,17 +8,13 @@ in {
     config = mkIf cfg.enable {
         programs.git = {
             enable = true;
-            userName = "Max van der Merwe";
+            userName = "Stefan van der Merwe";
             userEmail = "git@maxvdm.com";
             extraConfig = {
-              commit.gpgsign = true;
-              gpg.format = "ssh";
-              gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-              user.signingkey = "~/.ssh/max-git.pub";
-              init = { defaultBranch = "main"; };
-              core = {
+                init = { defaultBranch = "main"; };
+                core = {
                     excludesfile = "$NIXOS_CONFIG_DIR/scripts/gitignore";
-              };
+                };
             };
         };
     };
