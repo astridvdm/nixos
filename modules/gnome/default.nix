7e0e11d1df0ffca0fa19f81867188b244ff2f9b0
Max van der Merwe <git@maxvdm.com>
2023-12-12 21:43:37 +0200

    add gnome module

---
+++ modules/gnome/default.nix
@@ -0,0 +1,41 @@
+{ pkgs, lib, config, ... }:
+
+with lib;
+let cfg =
+    config.modules.gnome;
+    maintenance = pkgs.writeShellScriptBin "maintenance" ''${builtins.readFile ./maintenance}'';
+
+in {
+    options.modules.gnome = { enable = mkEnableOption "gnome"; };
+    config = mkIf cfg.enable {
+      # Enable the X11/Wayland windowing system.
+      services.xserver.enable = true;
+
+      # Enable the Gnome Desktop
+      services.xserver.displayManager.gdm.enable = true;
+      services.xserver.desktopManager.gnome.enable = true;
+
+      environment.gnome.excludePackages = (with pkgs; [
+        #gnome-photos
+        gnome-tour
+      ]) ++ (with pkgs.gnome; [
+        cheese # webcam tool
+        gnome-music
+        #gnome-terminal
+        gedit # text editor
+        epiphany # web browser
+        geary # email reader
+        evince # document viewer
+        gnome-characters
+        totem # video player
+        tali # poker game
+        iagno # go game
+        hitori # sudoku game
+        atomix # puzzle game
+      ]);
+
+      # Enable wayland flag for windows
+      environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+    };
+}
