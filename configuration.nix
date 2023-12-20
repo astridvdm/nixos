a6cf4b33f1c0f440c8a3587a3d8408f898be5dc0
Max van der Merwe <git@maxvdm.com>
2023-12-20 22:32:18 +0200

    Fix typos and remove nvram segment for KVM

--- configuration.nix
+++ configuration.nix
@@ -147,14 +147,8 @@
 
 
   # KVM Virtualization
+  virtualisation.libvirtd.enable = true;
 
-  # Adapted from /var/lib/libvirt/qemu.conf
-  # Note that AAVMF and OVMF are for Aarch64 and x86 respectively
-  nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]
-
-  virtualisation.libvirtd = {
-    enable = true;
-  };
 
   # NixOS Optimise
   boot.loader.systemd-boot.configurationLimit = 10;
