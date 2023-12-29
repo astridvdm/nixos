bf10d019440c31c41a3f8639ee857e92bbd291df
Max van der Merwe <git@maxvdm.com>
2023-12-29 21:58:40 +0200

    add swap

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -26,7 +26,9 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/175c3616-60a9-43bf-b370-3eaf0c3edb09"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
