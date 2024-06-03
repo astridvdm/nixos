f171fe445e43a1b930bb55f077bee1547acf2da8
Max van der Merwe <git@maxvdm.com>
2024-06-03 22:18:46 +0200

    configure swap for hera.

--- machines/hera/hardware-configuration.nix
+++ machines/hera/hardware-configuration.nix
@@ -29,8 +29,9 @@
 
   swapDevices = [ {
     device = "/var/lib/swapfile";
-    size = 32*1024;
+    size = 16*1024;
   } ];
+
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
