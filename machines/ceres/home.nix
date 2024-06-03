71f953d481c34157be3cb171c636ef353292385f
Max van der Merwe <git@maxvdm.com>
2024-06-03 22:08:17 +0200

    Update ceres configs.

--- machines/ceres/home.nix
+++ machines/ceres/home.nix
@@ -6,69 +6,59 @@
   home.homeDirectory = "/home/max";
  
  # User packages
-  home-manager.users.max = { pkgs, ... }: {
-    home.packages = with pkgs; [
-    ];
-     # Config SSH
-     programs.ssh = {
-       enable = true;
-       compression = true;
-
-       # Server conigs
-       matchBlocks = {
-         # Hetzer storage
-         "u334582.your-storagebox.de" = {
-         hostname = "u334582.your-storagebox.de";
-         user = "u334582";
-         port = 23;
-         identityFile = "/home/max/.ssh/hetzner-borg";
-         };
-         "github.com" = {
-         hostname = "github.com";
-         user = "git";
-         identityFile = "/home/max/.ssh/max-git";
-         };
-         "lux" = {
-         hostname = "172.16.0.212";
-         proxyJump = "100.96.163.55";
-         user = "brock";
-         identityFile = "/home/max/.ssh/max-a17";
-         };
-       };
-     };
-     programs.zsh.enable = true;
-     programs.starship = {
-       enable = true;
-       # Configuration written to ~/.config/starship.toml
-       settings = {
-       # add_newline = false;
-
-       # character = {
-       #   success_symbol = "[➜](bold green)";
-       #   error_symbol = "[➜](bold red)";
-       # };
-
-       # package.disabled = true;
-       };
-     };
-     # Configure git
-     programs.git = {
-       enable = true;
-       userName  = "Max van der Merwe";
-       userEmail = "git@maxvdm.com";
-
-       extraConfig = {
-         # Sign all commits using ssh key
-         commit.gpgsign = true;
-         gpg.format = "ssh";
-         gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-         user.signingkey = "~/.ssh/max-git.pub";
-       };
-     };
-     home.stateVersion = "24.05";
-     nixpkgs.config = import ./nixpkgs-config.nix;
+  home.packages = with pkgs; [ ];
+    # Config SSH
+    programs.ssh = {
+      enable = true;
+      compression = true;
+      # Server conigs
+      matchBlocks = {
+        # Hetzer storage
+        "u334582.your-storagebox.de" = {
+        hostname = "u334582.your-storagebox.de";
+        user = "u334582";
+        port = 23;
+        identityFile = "/home/max/.ssh/hetzner-borg";
+        };
+        "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "/home/max/.ssh/max-git";
+        };
+        "lux" = {
+        hostname = "172.16.0.212";
+        proxyJump = "100.96.163.55";
+        user = "brock";
+        identityFile = "/home/max/.ssh/max-a17";
+        };
+      };
     };
-
+    programs.zsh.enable = true;
+    programs.starship = {
+      enable = true;
+      # Configuration written to ~/.config/starship.toml
+      settings = {
+      # add_newline = false;
+      # character = {
+      #   success_symbol = "[➜](bold green)";
+      #   error_symbol = "[➜](bold red)";
+      # };
+      # package.disabled = true;
+      };
+    };
+    # Configure git
+    programs.git = {
+      enable = true;
+      userName  = "Max van der Merwe";
+      userEmail = "git@maxvdm.com";
+      extraConfig = {
+        # Sign all commits using ssh key
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        user.signingkey = "~/.ssh/max-git.pub";
+      };
+    };
   home.stateVersion = "24.05";
 
   # Let home Manager install and manage itself.
