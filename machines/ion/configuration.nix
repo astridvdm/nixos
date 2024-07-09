ed04cab4530f06f42d0598110b9a2fca4eefdcb7
Max van der Merwe <git@maxvdm.com>
2024-07-09 12:28:46 +0200

    Update nvidia config, update printer driver.

--- machines/ion/configuration.nix
+++ machines/ion/configuration.nix
@@ -33,7 +33,7 @@
     powerManagement.enable = true;
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -49,7 +49,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Increase vm count for Star Citizen
