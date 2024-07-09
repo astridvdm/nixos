c64cf6d323f2f261d30438625f39e212810c399b
Max van der Merwe <git@maxvdm.com>
2024-07-09 10:02:01 +0200

    Update flake, add options for ledger wallet.

--- machines/ion/configuration.nix
+++ machines/ion/configuration.nix
@@ -169,7 +169,7 @@
     # Name
     description = "Max";
     # Groups
-    extraGroups = [ "networkmanager" "wheel" "docker" "kvm" "libvirtd" "input" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "kvm" "libvirtd" "input" "plugdev" ];
     # Specify user shell
     shell = "/run/current-system/sw/bin/zsh";
     # User packages
@@ -244,6 +244,25 @@
 
   # Hardware crypto wallet manager
   hardware.ledger.enable = true;
+  
+  # #groups.plugdev = {};
+  # # Required for Ledger Live to detect Ledger Nano S via USB
+  # services.udev.extraRules = ''
+  # # firmware 1.6.0+
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="2b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="3b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="4b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1807", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1808", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0000", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="1011", MODE="0660", GROUP="plugdev"
+  # SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="1015", MODE="0660", GROUP="plugdev"
+
+  # ''
+  # ;
 
   # KVM Virtual machines
   virtualisation.libvirtd.enable = true;
