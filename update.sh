d212d18272bd491702e815780d35852fccd207b2
Max van der Merwe <git@maxvdm.com>
2024-07-23 12:00:52 +0200

    Update hera home file, add initual update script.

---
+++ update.sh
@@ -0,0 +1,59 @@
+git fetch
+
+git pull
+
+nix flake update
+
+git add *
+
+# git commit -m "Update systems"
+
+sudo nixos-rebuild boot
+
+# ssh ceres
+
+# cd /etc/nixos
+
+# git fetch
+
+# git pull
+
+# sudo nixos-rebuild boot
+
+# reboot
+
+# ssh polaris
+
+# cd /etc/nixos
+
+# git fetch
+
+# git pull
+
+# sudo nixos-rebuild boot
+
+# reboot
+
+# ssh ion
+
+# cd /etc/nixos
+
+# git fetch
+
+# git pull
+
+# sudo nixos-rebuild boot
+
+# reboot
+
+# ssh hera
+
+# cd /etc/nixos
+
+# git fetch
+
+# git pull
+
+# sudo nixos-rebuild boot
+
+# reboot
