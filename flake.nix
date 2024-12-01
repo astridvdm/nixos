ad255c0798ba9919ddaf582afda8c995edc305d2
Astrid van der Merwe <git@astridvdm.com>
2024-12-01 21:51:02 +0200

    Fix typo.

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     #  nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     #  home-manager.url = "github:nix-community/home-manager/release-24.05";
 
-    # Stable 24.05
+    # Stable 24.11
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
 
