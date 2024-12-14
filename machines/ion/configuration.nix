f19df311329a9fd1ce168273d4c7036729aba246
Astrid van der Merwe <git@astridvdm.com>
2024-12-14 16:57:41 +0200

    Update Steam config for ion.

--- machines/ion/configuration.nix
+++ machines/ion/configuration.nix
@@ -270,7 +270,12 @@
   programs.virt-manager.enable = true;
 
   # Steam
-  programs.steam.enable = true;
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+};
 
   # Docker
   virtualisation.docker.enable = true;
