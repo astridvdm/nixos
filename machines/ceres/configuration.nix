2aa90d1dc8b689c59565287684c3b73e98d24baf
Astrid van der Merwe <git@astridvdm.com>
2025-11-05 11:13:46 +0200

    Add samba config.

--- machines/ceres/configuration.nix
+++ machines/ceres/configuration.nix
@@ -304,6 +304,25 @@
    '';
  };
 
+  services.samba = {
+    package = pkgs.samba4Full;
+    # ^^ `samba4Full` is compiled with avahi, ldap, AD etc support (compared to the default package, `samba`
+    # Required for samba to register mDNS records for auto discovery
+    # See https://github.com/NixOS/nixpkgs/blob/592047fc9e4f7b74a4dc85d1b9f5243dfe4899e3/pkgs/top-level/all-packages.nix#L27268
+      enable = true;
+      openFirewall = true;
+      shares.ceres = {
+        path = "/ceres";
+        writable = "true";
+        #comment = "";
+      };
+  };
+  services.samba-wsdd = {
+    # This enables autodiscovery on windows since SMB1 (and thus netbios) support was discontinued
+    enable = true;
+    openFirewall = true;
+  };
+
   # NixOS Optimise
   boot.loader.systemd-boot.configurationLimit = 10;
   # boot.loader.grub.configurationLimit = 10;
